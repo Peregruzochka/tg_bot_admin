40d5dae7997e4e066f3dab6800a42d808e4c8652
Peregruzochka <86581077+Peregruzochka@users.noreply.github.com>
2025-02-03 12:06:39 +0300

    init

---
+++ src/main/java/ru/peregruzochka/tg_bot_admin/TgBotAdminApplication.java
@@ -0,0 +1,13 @@
+package ru.peregruzochka.tg_bot_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgBotAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/peregruzochka/tg_bot_admin/TgBotAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.peregruzochka.tg_bot_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
