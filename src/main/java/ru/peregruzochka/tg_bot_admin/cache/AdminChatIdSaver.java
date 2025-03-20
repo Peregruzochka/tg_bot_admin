5dd76ad908f0c6a3a0413ee71494d3fc1dc0c7dd
Peregruzochka <peregruzochka48@mail.ru>
2025-03-20 22:17:01 +0300

    при перезапуске admin bot  -> клиент по умолчанию

--- src/main/java/ru/peregruzochka/tg_bot_admin/cache/AdminChatIdSaver.java
+++ src/main/java/ru/peregruzochka/tg_bot_admin/cache/AdminChatIdSaver.java
@@ -1,12 +1,24 @@
 package ru.peregruzochka.tg_bot_admin.cache;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
+import ru.peregruzochka.tg_bot_admin.tools.AuthUserChecker;
 
 @Component
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class AdminChatIdSaver {
+
+    private final AuthUserChecker authUserChecker;
+
+    @PostConstruct
+    void init() {
+        chatId = authUserChecker.getUserIds().get(0);
+    }
+
     private Long chatId;
 }
